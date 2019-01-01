13e63287b06eda22a461c6055038ac7d5f4de03c
MasayaKataoka <ms.kataoka@gmail.com>
2019-01-01 23:02:04 +0900

    enable recieve packet

--- include/ros_mbed_bridge/udp_server.h
+++ include/ros_mbed_bridge/udp_server.h
@@ -1,9 +1,33 @@
 #ifndef UDP_SERVER_H_INCLUDED
 #define UDP_SERVER_H_INCLUDED
 
+//headers in STL
+#include <functional>
+#include <vector>
+#include <mutex>
+
+//headers in boost
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/shared_ptr.hpp>
+
+#define BUFFER_SIZE 1024
+
 class UdpServer
 {
-
+public:
+    UdpServer(int port);
+    ~UdpServer();
+    void registerCallback(std::function<void(boost::property_tree::ptree)> func);
+    void run();
+    const int port;
+private:
+    std::vector<std::function<void(boost::property_tree::ptree)> > callback_funcs_;
+    boost::array<char,BUFFER_SIZE> recv_buf_;
+    std::mutex mtx_;
+    void receive(const boost::system::error_code&, std::size_t len);
 };
 
 #endif  //UDP_SERVER_H_INCLUDED
--- src/udp_server.cpp
+++ src/udp_server.cpp
@@ -1,1 +1,54 @@
 #include <ros_mbed_bridge/udp_server.h>
+
+UdpServer::UdpServer(int port): port(port)
+{
+
+}
+
+UdpServer::~UdpServer()
+{
+
+}
+
+void UdpServer::run()
+{
+    using boost::asio::ip::udp;
+    boost::asio::io_service io_service;
+    mtx_.lock();
+    try
+    {
+        udp::socket socket(io_service, udp::endpoint(udp::v4(), port));
+        udp::endpoint remote_endpoint;
+        boost::system::error_code error;
+        socket.async_receive_from(boost::asio::buffer(recv_buf_), remote_endpoint, boost::bind(&UdpServer::receive,this,_1,_2));
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    io_service.run();
+    mtx_.unlock();
+    return;
+}
+
+void UdpServer::registerCallback(std::function<void(boost::property_tree::ptree)> func)
+{
+    mtx_.lock();
+    callback_funcs_.push_back(func);
+    mtx_.unlock();
+    return;
+}
+
+void UdpServer::receive(const boost::system::error_code&, std::size_t len)
+{
+    std::string recv_string = recv_buf_.data();
+    using namespace boost::property_tree;
+    ptree pt;
+    read_json(recv_string, pt);
+    for(auto func_ptr = callback_funcs_.begin(); func_ptr != callback_funcs_.end(); func_ptr++)
+    {
+        std::function<void(boost::property_tree::ptree)> func = *func_ptr;
+        func(pt);
+    }
+    return;
+}
